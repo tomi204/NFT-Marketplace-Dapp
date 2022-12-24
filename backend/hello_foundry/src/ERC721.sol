d987a2fd1e46e8cdbbd3110a0713087ba9c012de
tomi204 <tomi204@outlook.com>
2022-12-24 02:20:20 -0300

    definiendo librerias de los contratos

---
+++ backend/hello_foundry/src/ERC721.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "";
+
+contract NFT is ERC721{
+
+}
---
+++ backend/hello_foundry/src/Marketplace.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "";
+contract marketPlace is Ownable, IERC721 {
+    constructor() {
+        
+    }
+}
