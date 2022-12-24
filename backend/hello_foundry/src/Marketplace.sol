211b909e79656c80183b225d39d56839b7cb8ef0
tomi204 <tomi204@outlook.com>
2022-12-24 02:44:43 -0300

    item struct y constructor

--- backend/hello_foundry/src/Marketplace.sol
+++ backend/hello_foundry/src/Marketplace.sol
@@ -5,7 +5,30 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "";
 contract marketPlace is Ownable, IERC721 {
-    constructor() {
+
+    //@dev state variables
+    address payable public immutable owner; //owner of the contract
+    uint64 public immutable fee; //marketplace fee
+    uint256 public itemCount; // item count
+
+    //@dev struct of items
+
+    struct item{
+        uint256 id;
+        address nft;
+        uint256 tokenId;
+        address payable seller;
+        bool sold;
+        uint256 price;
+        string name;
+        string description;
+        string image;
+    }
+
+    constructor(uint64 _fee) {
+        owner = payable(msg.sender);
+        fee = _fee;
+    }
         
     }
-}
+
