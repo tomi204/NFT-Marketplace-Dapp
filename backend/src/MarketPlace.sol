819460ca81be1eb7a3bfa6d0525eb32ee5137e35
tomi204 <tomi204@outlook.com>
2022-12-24 18:40:31 -0300

    fix foundry and fix wagmi version

---
+++ backend/src/MarketPlace.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+//@Dev errors
+error NotApprovedForMarketplace();
+
+contract marketPlace is ReentrancyGuard {
+    //@dev state variables
+    address payable public immutable owner; //owner of the contract
+    uint64 public immutable fee; //marketplace fee
+    uint256 public itemCount; // item count
+
+    //@dev constructor
+    constructor(uint64 _fee) {
+        owner = payable(msg.sender);
+        fee = _fee;
+    }
+
+    //@dev struct of items
+
+    struct item {
+        uint256 id;
+        address nft;
+        uint256 tokenId;
+        address payable seller;
+        bool sold;
+        uint256 price;
+        string name;
+        string description;
+        string image;
+    }
+
+    //@dev mappings
+    mapping(uint256 => item) public items; // item id to item
+    mapping(uint256 => uint256) _security; // security front running
+
+    //@modifiers
+
+    //@dev security front running modifier
+    modifier securityFrontRunning(uint256 _itemId) {
+        item storage Item = items[_itemId];
+        require(
+            _security[_itemId] == 0 || _security[_itemId] > block.number,
+            "error security"
+        );
+
+        _security[_itemId] = block.number;
+        _;
+    }
+
+    //@dev onlyOwner modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    //@dev events
+    event listed(
+        uint256 id,
+        address nft,
+        uint256 tokenId,
+        uint256 price,
+        address seller
+    );
+
+    //@dev functions
+
+    function listNFT(
+        address _nft,
+        uint256 _tokenId,
+        uint256 _price,
+        string memory _name,
+        string memory _description,
+        string memory _image
+    ) public nonReentrant {
+        require(_price > 0, "price must be greater than 0");
+        IERC721 nft = IERC721(_nft); // cast address to IERC721
+        if (nft.ownerOf(_tokenId) == msg.sender) {
+            // check if the msg.sender is the owner of the NFT
+
+            if (nft.getApproved(_tokenId) != address(this)) {
+                revert NotApprovedForMarketplace();
+            }
+            itemCount++; // increment the item count
+            items[itemCount] = item( // add the item to the items mapping
+                itemCount,
+                _nft,
+                _tokenId,
+                payable(msg.sender),
+                false,
+                _price,
+                _name,
+                _description,
+                _image
+            );
+            emit listed(itemCount, address(_nft), _tokenId, _price, msg.sender); // emit the listed event
+        } else {
+            revert("you are not the owner of this NFT");
+        }
+    }
+
+    //dev buy function
+    function buyNFT(uint256 _itemId)
+        public
+        payable
+        nonReentrant
+        securityFrontRunning(_itemId)
+    {
+        item storage Item = items[_itemId]; // get the item from the items mapping
+        require(Item.sold == false, "item already sold"); // check if the item is already sold
+        require(msg.value == (Item.price * fee) / 100, "insufficient funds"); // check if the msg.value is greater than or equal to the item price
+        IERC721(Item.nft).safeTransferFrom(
+                Item.seller,
+                msg.sender,
+                Item.tokenId
+            ); // trasnfer the nft to the buyer        Item.sold = true; // set the item sold state to true
+        uint256 feeAmount = (Item.price * fee) / 100; // calculate the fee amount
+        uint256 sellerAmount = Item.price - feeAmount; // calculate the seller amount
+        payable(owner).transfer(feeAmount); // transfer the fee amount to the owner
+        payable(Item.seller).transfer(sellerAmount); // transfer the seller amount to the seller
+        delete _security[_itemId]; // delete the security front running
+        delete items[_itemId]; // delete the item from the items mapping
+    }
+
+    //@dev cancel function
+
+    function cancelSale(uint256 _itemId)
+        public
+        nonReentrant
+        securityFrontRunning(_itemId)
+    {
+        item storage Item = items[_itemId]; // get the item from the items mapping
+        require(Item.sold == false, "item already sold"); // check if the item is already sold
+        require(Item.seller == msg.sender, "you are not the seller"); // check if the msg.sender is the seller
+        delete items[_itemId]; // delete the item from the items mapping
+    }
+
+    //@dev withdraw founds function
+
+    function withdrawFunds() public onlyOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+
+    //@dev get items for front end function
+
+    function getItems(uint256 _itemId) public view returns (item memory) {
+        item storage itemsList = items[_itemId]; // get specific item from the items mapping
+        return itemsList; // return the item
+    }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,27 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, goerli } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    goerli,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
   ],
   [
     alchemyProvider({
-      // This is Alchemy's default API key.
-      // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: "EypKcb615zspS9zr3YpMF1zNodFvArmW",
     }),
     publicProvider(),
   ]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+  appName: "RainbowKit + wagmi + Next.js",
   chains,
 });
 
