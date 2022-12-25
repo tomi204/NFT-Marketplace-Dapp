ba33953afb430fbe3d88b1b5bad3dce727225e3f
tomi204 <tomi204@outlook.com>
2022-12-25 04:36:35 -0300

    navbar styles & pages

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className={styles.mainDiv}>
+      <li>
+        <Link href="/">
+          <a className={styles.link}>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <a className={styles.link}>Sell NFT</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/">
+          <a className={styles.link}>Wallet</a>
+        </Link>
+      </li>
+
       <ConnectButton />
     </div>
   );
