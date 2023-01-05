7728d689a5bda77e826636edfd4fae08fe89cb73
tomi204 <tomi204@outlook.com>
2023-01-05 01:43:00 -0300

    footer style

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import {
+  FaGithub,
+  FaGitlab,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa";
 import styles from "./footer.module.css";
+import { Link } from "next/link";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <a href="https://github.com/tomi204">
+        <FaGithub className={styles.icon} />
+      </a>
+      <a href="https://twitter.com/tomiioliver">
+        <FaTwitter className={styles.icon} />
+      </a>
       <a target="_blank" rel="noopener noreferrer">
         Made with ❤️ by Tomi204
+      </a>
+      <a href="https://www.linkedin.com/in/tomi204/">
+        <FaLinkedinIn className={styles.icon} />
+      </a>
+      <a href="https://www.instagram.com/tomi204_/">
+        <FaInstagram className={styles.icon} />
       </a>
     </footer>
   );
--- pages/sell.jsx
+++ pages/sell.jsx
@@ -3,12 +3,15 @@ import { Sell } from "./../components/api/ListNFT";
 import Navbar from "./../components/navbar/Navbar";
 import { useIsMounted } from "./../components/utils/mounted";
 import styles from "../styles/sell.module.css";
+import Footer from "./../components/footer/Footer";
 const SellNFT = () => {
   const mounted = useIsMounted();
   return (
     <div className={styles.pageSell}>
       <Navbar />
+      <br />
       {mounted ? <Sell /> : null}
+      <Footer />
     </div>
   );
 };
