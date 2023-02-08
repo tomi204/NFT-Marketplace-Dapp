ed355386361251a187bcacb52e354f233e4038fd
tomas <tomi204@outlook.com>
2023-02-08 17:48:21 -0300

    tests in foundry

--- backend/src/ERC721.sol
+++ backend/src/ERC721.sol
@@ -2,21 +2,20 @@
 pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFT is ERC721{
+contract NFT is ERC721 {
     uint256 public tokenCounter;
-    
+
     constructor() ERC721("Bored ape 204", "Bored Ape #204") {
         _mint(msg.sender, 1);
     }
-    
-      function mint() public
-    {
-        tokenCounter  += 1;
+
+    function mint() public {
+        tokenCounter += 1;
         _mint(msg.sender, tokenCounter);
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8";
+        return
+            "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8";
     }
-
 }
--- backend/test/Marketplace.sol
+++ backend/test/Marketplace.sol
@@ -4,17 +4,75 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import {marketPlace} from "../src/MarketPlace.sol";
 import {NFT} from "../src/ERC721.sol";
-contract MarketplaceTest is Test{
+
+contract MarketplaceTest is Test {
     marketPlace public MarketPlace;
     NFT public nft;
 
     function setUp() public {
-        MarketPlace  = new marketPlace();
+        MarketPlace = new marketPlace();
         nft = new NFT();
     }
 
-    function test() public {
+    function testCancel() public {
         nft.approve(address(MarketPlace), 1);
-        MarketPlace.listNFT(address(nft), 1, 200, "hola", "this nft is for sale", "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8");
+        MarketPlace.listNFT(
+            address(nft),
+            1,
+            1 ether,
+            "hola",
+            "this nft is for sale",
+            "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8"
+        );
+        MarketPlace.cancelSale(1);
+    }
+
+    function testAuction() public {
+        nft.approve(address(MarketPlace), 1);
+        MarketPlace.listNFT(
+            address(nft),
+            1,
+            1 ether,
+            "hola",
+            "this nft is for sale",
+            "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8"
+        );
+        MarketPlace.listNFTAuction(
+            address(nft),
+            1,
+            1 ether,
+            20,
+            "hola",
+            "this nft is for sale",
+            "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8"
+        );
+        MarketPlace.cancelAuction(1);
+    }
+
+    function testBuy() public {
+        nft.approve(address(MarketPlace), 1);
+        MarketPlace.listNFT(
+            address(nft),
+            1,
+            1 ether,
+            "hola",
+            "this nft is for sale",
+            "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8"
+        );
+        MarketPlace.buyNFT(1);
+    }
+
+    function testBid() public {
+        nft.approve(address(MarketPlace), 1);
+        MarketPlace.listNFTAuction(
+            address(nft),
+            1,
+            1 ether,
+            20,
+            "hola",
+            "this nft is for sale",
+            "https://ipfs.io/ipfs//QmdRpbUC8TjJ1b9tZWPRxBvbCsgJisWEuN4dcXFW4Mvrh8"
+        );
+        MarketPlace.bidAuction(1);
     }
 }
